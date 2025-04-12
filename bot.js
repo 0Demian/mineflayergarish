c7138d2f643447a25774706dc8ae45a4e77b5ffa
Yahya <35798460+0Demian@users.noreply.github.com>
2025-04-12 16:08:11 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,1 +1,12 @@
+const mineflayer = require('mineflayer');
 
+const bot = mineflayer.createBot({
+  host: process.env.MC_HOST,
+  port: parseInt(process.env.MC_PORT),
+  username: process.env.BOT_NAME,
+});
+
+bot.on('chat', (username, message) => {
+  if (username === bot.username) return;
+  bot.chat(`Hello ${username}, you said: ${message}`);
+});
